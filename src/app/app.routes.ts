f6a4cb49eedd7ebe3126245d67d7f75f5224e6d0
Andres <andres.edu.glez@gmail.com>
2025-10-20 12:13:39 -0600

    se agrego la vista de agrgar especies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-// Asegúrate de que esta ruta de importación sea correcta
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { SpycesComponent } from './pages/spyces/spyces.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingPageComponent }, // Muestra la LandingPage en la raíz
-  // { path: 'otro', component: OtroComponent }, // Ejemplo de otra ruta
-  { path: '**', redirectTo: '' } // Redirige cualquier ruta no encontrada a la raíz
+  { path: '', component: LandingPageComponent }, 
+  { path: 'spyces', component: SpycesComponent },
+  { path: '**', redirectTo: '' } 
 ];
