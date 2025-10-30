5c03f499e564d82fe4fa4281b064da981c913193
Andres <andres.edu.glez@gmail.com>
2025-10-29 21:59:16 -0600

    se creo el componente de coleccion cart

---
+++ src/app/core/components/coletcion-cart/colection-cart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-coletcion-cart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './coletcion-cart.html',
+  styleUrls: ['./coletcion-cart.css']
+})
+export class NavBarComponent {
+
+}
---
+++ src/app/core/components/coletcion-cart/coletcion-cart.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coletcion-cart',
+  imports: [],
+  templateUrl: './coletcion-cart.html',
+  styleUrl: './coletcion-cart.css'
+})
+export class ColetcionCart {
+
+}
