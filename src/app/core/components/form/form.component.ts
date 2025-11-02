31541ed07b0d0adb6ee5ffe9dc940b7aaaed7e4d
Andres <andres.edu.glez@gmail.com>
2025-11-01 18:24:13 -0600

    creacion del segundo componente

---
+++ src/app/core/components/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form.html',
+  styleUrls: ['./form.css']
+})
+export class FormComponent {
+
+}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { ImageLoginComponent } from '../../core/components/image-login/image-login.component';
-
+import { FormComponent } from '../../core/components/form/form.component';
 @Component({
   selector: 'app-login',
-  imports: [ImageLoginComponent],
+  imports: [ImageLoginComponent,FormComponent],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
