0199f21e1cf3c92ca2f284fe83bca4d2f4f493c1
Andres <andres.edu.glez@gmail.com>
2025-11-01 22:43:36 -0600

    adicion de los estilos de los componentes de rgisstro

---
+++ src/app/core/components/RegisterC/image-register/image-register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-image-register',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-register.html',
+  styleUrls: ['./image-register.css']
+})
+export class ImageRegisterComponent {
+
+}
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ImageRegisterComponent } from '../../core/components/RegisterC/image-register/image-register.component';
+import { Form1Component } from '../../core/components/RegisterC/form1/form1.component';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ImageRegisterComponent, Form1Component],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
