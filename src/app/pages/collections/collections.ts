b56c9609b02e11c4d5acdedf44ae50ac607737b6
cesar160 <ctoledovillarreal@gmail.com>
2025-11-27 11:54:47 -0600

    actualice modulo de collections

--- src/app/pages/collections/collections.ts
+++ src/app/pages/collections/collections.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../../core/components/nav-bar/nav-bar.component';
-import { ColetcionCartComponent } from '../../core/components/colletcion-cart/collection-cart.component';
+import { CollectionService } from '../../services/collection.service';
+import { Router } from '@angular/router';
+import { CollectionCartComponent } from '../../core/components/collection-cart/collection-cart.component';
 
 @Component({
-  selector: 'app-collections',
+  selector: 'app-collection-r',
   standalone: true,
-  imports: [CommonModule, NavBarComponent,ColetcionCartComponent],
+  imports: [CommonModule, NavBarComponent, CollectionCartComponent],
   templateUrl: './collections.html',
   styleUrl: './collections.css'
 })
-export class Collections {
+export class CollectionR implements OnInit {
+  collections: any[] = [];
 
+  constructor(
+    private collectionService: CollectionService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loadCollections();
+  }
+
+  loadCollections(): void {
+    this.collectionService.getAllCollectionsWithSpecimens().subscribe({
+      next: (data: any[]) => {
+        console.log('✅ Colecciones cargadas:', data);
+        this.collections = data;
+        console.log('✅ Colecciones cargadas (con imageUrl):', this.collections);
+      },
+      error: (error: any) => {
+        console.error('❌ Error al cargar colecciones:', error);
+      }
+    });
+  }
+
+  onCollectionSelected(collectionId: string): void {
+    this.router.navigate(['/specimens/collection', collectionId]);
+  }
 }
