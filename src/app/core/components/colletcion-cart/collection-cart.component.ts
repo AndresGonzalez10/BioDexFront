66a958142e34fd3496d89988625812792a5872c8
cesar160 <ctoledovillarreal@gmail.com>
2025-11-27 11:44:42 -0600

    Implementacion de la API en collection card

--- src/app/core/components/colletcion-cart/collection-cart.component.ts
+++ src/app/core/components/colletcion-cart/collection-cart.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-colletcion-cart',
+  selector: 'app-collection-cart',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './colletcion-cart.html',
-  styleUrls: ['./colletcion-cart.css']
+  templateUrl: './collection-cart.html',
+  styleUrls: ['./collection-cart.css']
 })
-export class ColetcionCartComponent {
+export class CollectionCartComponent {
+  @Input() collection: any;
+  @Output() collectionSelected = new EventEmitter<string>();
 
+  onCollectionClick(): void {
+    this.collectionSelected.emit(this.collection.id);
+  }
 }
